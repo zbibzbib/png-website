c239af2b3a50947c4a3f1a1b51a4090deda99179
JOJO <JoJo2021up@protonmail.com>
2021-11-17 14:28:38 +0800

    Images compressing and font resize

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -37,9 +37,6 @@ import row01 from 'assets/home/row01.png';
 import row02 from 'assets/home/row02.png';
 import row03 from 'assets/home/row03.png';
 import areaBg from 'assets/home/area_bg.png';
-import bottom01 from 'assets/home/bottom_01.png';
-import bottom02 from 'assets/home/bottom_02.png';
-import bottom03 from 'assets/home/bottom_03.png';
 
 export const Home: React.FC = () => {
 
@@ -60,7 +57,7 @@ export const Home: React.FC = () => {
       <Box bg={`#09090b url(${bannerBg}) left top no-repeat`} bgSize="100% 100%">
         <Box maxW={1200} margin="0 auto"  >
 
-          <Flex p="50px 10px" alignItems="center" justifyContent="space-between" h="50px">
+          <Flex p="0 10px" alignItems="center" justifyContent="space-between" h="80px">
             <Box flex={1} >
               <Link><Image src={logoLight} width="140px" /></Link>
             </Box>
@@ -92,13 +89,13 @@ export const Home: React.FC = () => {
 
           <Center h={clientH - 100}>
             <Flex display={{ base: "none", md: "flex" }} w={{ base: "100%", md: 1200 }} alignItems="center" justifyContent="space-between" overflow="hidden">
-              <Box w={{ base: "100%", md: 500 }} className="top_left" p="0 10px">
-                <Heading as="h1" size="4xl" isTruncated>PENGUIN</Heading>
-                <Text fontSize="4xl" fontWeight="bold" mt="10px" >One-stop DeFi application platform</Text>
-                <Text className="gray_text" mt="40px" fontSize="lg">
+              <Box w="100%" className="top_left" p="0 10px">
+                <Heading as="h1" fontSize="80px" isTruncated>PENGUIN</Heading>
+                <Text fontSize="40px" fontWeight="bold" mt="10px" >One-stop DeFi application platform</Text>
+                <Text className="gray_text" mt="51px" fontSize="24px" lineHeight="40px">
                   Penguin is the automatic market-making protocol based on Solana.
                 </Text>
-                <PrimaryButton mt="60px" size="lg"><a href="#">Launch App</a></PrimaryButton>
+                <PrimaryButton mt="88px" size="lg"><a href="#">Launch App</a></PrimaryButton>
               </Box>
               <Image src={homeBanner01} className="top_right" />
             </Flex>
@@ -115,7 +112,7 @@ export const Home: React.FC = () => {
                 <Text className="gray_text" mt="30px" fontSize="lg">
                   Penguin is the automatic market-making protocol based on Solana.
                 </Text>
-                <PrimaryButton mt="60px" size="lg"><a href="http://www.baidu.com">Launch App</a></PrimaryButton>
+                <PrimaryButton mt="60px" size="lg"><a href="#">Launch App</a></PrimaryButton>
               </Box>
             </Box>
           </Center>
@@ -123,46 +120,47 @@ export const Home: React.FC = () => {
       </Box >
 
       <Box w="100%" margin="0 auto" padding="0 10px" textAlign="center" position="relative" >
-        <Box p="100px 0">
-          <Text fontSize="4xl" fontWeight="bold">PENGUIN swap</Text>
-          <Text className="gray_text" mt="20px">
+        <Box p="140px 0 100px 0">
+          <Text fontSize="40px" fontWeight="bold">PENGUIN swap</Text>
+          <Text className="gray_text" fontSize="18px" lineHeight="29px" mt="20px">
             PENGUIN adopts the mechanism of Automatic Market Maker (AMM) and aims to achieve self-driven liquidity creation.
           </Text>
         </Box>
-        <Image src={areaBg} position="absolute" right="0" top={{ base: 350, md: 220 }} w="50%" h={{ base: "auto%", md: 280 }} />
+        <Image src={areaBg} position="absolute" right="0" top={{ base: 350, md: 280 }} w="50%" h={{ base: "auto%", md: 280 }} />
         <Image src={homeBanner02} width="1200px" margin="0 auto" position="relative" />
       </Box>
+      <Box h="96px" w="100%" position="relative" mt="-96px" bgGradient="linear-gradient(180deg,transparent 0%, #151a1d 61%)"></Box>
 
       <Box bg="#151A1D">
-        <Flex maxW={1200} margin="0 auto" justifyContent="space-between" p={"150px 10px"} >
+        <Flex maxW={1200} margin="0 auto" justifyContent="space-between" p={"250px 10px"} >
           <Image src={activeItem === 1 ? homeBanner03 : activeItem === 2 ? slide02 : slide03} w={{ base: "100%", md: 500 }} margin={{ base: "0 auto", xl: "" }} />
           <Box ml="100px" display={{ base: "none", xl: "block" }} >
             <Box p="20px 0">
-              <Text fontSize="4xl" fontWeight="bold">Safe fast and less slippage</Text>
-              <Text className="gray_text" fontSize="xl" p="40px 0">
+              <Text fontSize="40px" fontWeight="bold">Safe fast and less slippage</Text>
+              <Text className="gray_text" fontSize="18px" p="36px 0 60px 0">
                 leading currency asset exchange on solana
               </Text>
             </Box>
 
             <List spacing={30}>
               <ListItem className={activeItem === 1 ? "safe_item safe_item_active" : "safe_item"} onClick={() => onItemChange(1)}   >
-                <Flex alignItems="center" justifyContent="flex-start" p="30px" bg="#1f2326" borderRadius="20px" >
+                <Flex alignItems="center" justifyContent="flex-start" p="25px 36px" bg="#1f2326" borderRadius="20px" >
                   <Image src={item01} width="50px" />
-                  <Text ml="20px" fontSize="xl" lineHeight={1}>In Solana, use a far more granular verifiable delay function, a SHA 256 hash chain, to checkpoint the ledger and coordinate consensus.</Text>
+                  <Text ml="20px" fontSize="16px" lineHeight="1">In Solana, use a far more granular verifiable delay function, a SHA 256 hash chain, to checkpoint the ledger and coordinate consensus.</Text>
                 </Flex>
               </ListItem>
 
               <ListItem className={activeItem === 2 ? "safe_item safe_item_active" : "safe_item"} onClick={() => onItemChange(2)}   >
-                <Flex alignItems="center" justifyContent="flex-start" p="30px" bg="#1f2326" borderRadius="20px" >
+                <Flex alignItems="center" justifyContent="flex-start" p="25px 36px" bg="#1f2326" borderRadius="20px" >
                   <Image src={item02} width="50px" />
-                  <Text ml="20px" fontSize="xl">The resulting cluster wouldn"t be just 10 times faster, or a 100 times, or a 1,000 times, but 10,000 times faster, right out of the gate!</Text>
+                  <Text ml="20px" fontSize="16px">The resulting cluster wouldn"t be just 10 times faster, or a 100 times, or a 1,000 times, but 10,000 times faster, right out of the gate!</Text>
                 </Flex>
               </ListItem>
 
               <ListItem className={activeItem === 3 ? "safe_item safe_item_active" : "safe_item"} onClick={() => onItemChange(3)}   >
-                <Flex alignItems="center" justifyContent="flex-start" p="30px" bg="#1f2326" borderRadius="20px" >
+                <Flex alignItems="center" justifyContent="flex-start" p="25px 36px" bg="#1f2326" borderRadius="20px" >
                   <Image src={item03} width="50px" />
-                  <Text ml="20px" fontSize="xl">lowest platform transaction fees，user can save more</Text>
+                  <Text ml="20px" fontSize="16px">lowest platform transaction fees，user can save more</Text>
                 </Flex>
               </ListItem>
 
@@ -172,29 +170,29 @@ export const Home: React.FC = () => {
 
         <Box bg="#151A1D" maxW={1200} margin="0 auto" p="10px" textAlign="center" >
           <Box>
-            <Text fontSize="4xl" fontWeight="bold">Features</Text>
-            <Text className="gray_text" mt="20px">
+            <Text fontSize="40px" fontWeight="bold">Features</Text>
+            <Text className="gray_text" fontSize="18px" mt="20px">
               What we listed and under construction
             </Text>
           </Box>
 
           <Flex justifyContent="space-between" p={{ base: "50px 0 ", md: "150px 0" }}  >
-            <Box w={{ base: "30%", md: 200 }} className="feedback_up">
-              <Image src={row01} borderRadius="20px" width="140px" margin="0 auto" />
+            <Box w={{ base: "30%", md: 200 }}>
+              <Image src={row01} borderRadius="20px" width="140px" margin="0 auto" className="feedback_up" />
               <Box textAlign="center">
                 <Text fontSize="2xl" fontWeight="bold">Swap</Text>
                 <Text className="gray_text">safe fast and lower fees  </Text>
               </Box>
             </Box>
-            <Box w={{ base: "30%", md: 200 }} className="feedback_up" al>
-              <Image src={row02} borderRadius="20px" width="140px" margin="0 auto" />
+            <Box w={{ base: "30%", md: 200 }} >
+              <Image src={row02} borderRadius="20px" width="140px" margin="0 auto" className="feedback_up" />
               <Box textAlign="center">
                 <Text fontSize="2xl" fontWeight="bold">Pools</Text>
                 <Text className="gray_text">multiple trades piar supported</Text>
               </Box>
             </Box>
-            <Box w={{ base: "30%", md: 200 }} className="feedback_up">
-              <Image src={row03} borderRadius="20px" width="140px" margin="0 auto" />
+            <Box w={{ base: "30%", md: 200 }}>
+              <Image src={row03} borderRadius="20px" width="140px" margin="0 auto" className="feedback_up" />
               <Box textAlign="center">
                 <Text fontSize="2xl" fontWeight="bold">Farms</Text>
                 <Text className="gray_text">easy, sustainable yield farming for you</Text>
@@ -204,60 +202,64 @@ export const Home: React.FC = () => {
         </Box>
       </Box>
 
-      <Box bgGradient="linear-gradient(30deg, #000 5%, #182c33 15%, #11412f 20%, #11412f 30%, #000 60%)" pt="150px">
-        <Center><Text fontSize="4xl" fontWeight="bold" textAlign="center">Join us and let's PNG</Text></Center>
-        <Center><Text className="gray_text" textAlign="center">gives users the ability to swap, provide liquidity, farm to gain system.</Text></Center>
-        <Center p="38px 0 85px 0"><PrimaryButton size="lg"><a href="#">Launch App</a></PrimaryButton></Center >
+      <Box bg={`#09090b url(${bannerBg}) left top no-repeat`} bgSize="100% 100%">
+        {/* bgGradient="linear-gradient(30deg, #000 5%, #182c33 15%, #11412f 20%, #11412f 30%, #000 60%)"  */}
+        <Box pt="150px">
+          <Center><Text fontSize="40px" fontWeight="bold" textAlign="center">Join us and let's PNG</Text></Center>
+          <Center><Text className="gray_text" fontSize="18px" textAlign="center">gives users the ability to swap, provide liquidity, farm to gain system.</Text></Center>
+          <Center p="38px 0 85px 0"><PrimaryButton size="lg"><a href="#">Launch App</a></PrimaryButton></Center >
+        </Box>
+        <Box h="50px" w="100%" bgGradient="linear-gradient(-180deg,transparent,#000 70%)"></Box>
 
-        ’
-      </Box>
-      <Box bg="#000" p="150px 0">
-        <Box maxW={1200} margin="0 auto" p="0 10px" >
-          <Flex flexDirection={{ base: "column", md: "row" }} justifyContent="space-between">
-            <Box>
-              <Image src={logoLight} width="140px" />
-            </Box>
-            <Flex flex="1" margin={{ base: "40px 0", md: 0 }} p="0 20px" justifyContent={{ base: "space-between", md: "space-around" }}>
-              <VStack
-                spacing={3}
-                align="stretch"
-                className="gray_text bottom_links"
-              >
-                <Box fontWeight="bold" color="#fff" mb="10px">App</Box>
-                <Link>Swap</Link>
-                <Link>Pools</Link>
-                <Link>Stake</Link>
-              </VStack>
+        <Box bg="#000" p="100px 0 150px 0">
+          <Box maxW={1200} margin="0 auto" p="0 10px" >
+            <Flex flexDirection={{ base: "column", md: "row" }} justifyContent="space-between">
+              <Box>
+                <Image src={logoLight} width="140px" />
+              </Box>
+              <Flex flex="1" margin={{ base: "40px 0", md: 0 }} p="0 20px" justifyContent={{ base: "space-between", md: "space-around" }}>
+                <VStack
+                  spacing={3}
+                  align="stretch"
+                  className="bottom_links"
+                >
+                  
+                  <Box fontWeight="bold" fontSize="18px" color="#fff" mb="10px">App</Box>
+                  <Link>Swap</Link>
+                  <Link>Pools</Link>
+                  <Link>Stake</Link>
+                </VStack>
 
-              <VStack
-                spacing={3}
-                align="stretch"
-                className="gray_text bottom_links"
-              >
-                <Box fontWeight="bold" color="#fff" mb="10px">Resources</Box>
-                <Link>Docs</Link>
-                <Link>Github</Link>
-                <Link>Coingecko</Link>
-              </VStack>
+                <VStack
+                  spacing={3}
+                  align="stretch"
+                  className="bottom_links"
+                >
+                  <Box fontWeight="bold" fontSize="18px" color="#fff" mb="10px">Resources</Box>
+                  <Link>Docs</Link>
+                  <Link>Github</Link>
+                  <Link>Coingecko</Link>
+                </VStack>
 
-              <VStack
-                spacing={3}
-                align="stretch"
-                className="gray_text bottom_links"
-              >
-                <Box fontWeight="bold" color="#fff" mb="10px">Connect</Box>
-                <Link>Blog </Link>
-                <Link>Twitter</Link>
-                <Link>Discord</Link>
-              </VStack>
+                <VStack
+                  spacing={3}
+                  align="stretch"
+                  className="bottom_links"
+                >
+                  <Box fontWeight="bold" fontSize="18px" color="#fff" mb="10px">Connect</Box>
+                  <Link>Blog </Link>
+                  <Link>Twitter</Link>
+                  <Link>Discord</Link>
+                </VStack>
+              </Flex>
+              <Flex>
+                <Link className="bottom_icon bottom01 feedback_links_up"></Link>
+                <Link className="bottom_icon bottom02 feedback_links_up"></Link>
+                <Link className="bottom_icon bottom03 feedback_links_up"></Link>
+              </Flex>
+
             </Flex>
-            <Flex>
-              <Link className="bottom_icon bottom01 feedback_up"></Link>
-              <Link className="bottom_icon bottom02 feedback_up"></Link>
-              <Link className="bottom_icon bottom03 feedback_up"></Link>
-            </Flex>
-
-          </Flex>
+          </Box>
         </Box>
       </Box>
     </Box >
